1eb4618c3605e2ea60aec982adc7664c597592bd
dsauncy <dsauncy@wgu.edu>
2024-06-06 17:31:29 -0700

    Project Complete

--- main.py
+++ main.py
@@ -60,7 +60,6 @@ async def get_root():
     return {"greeting": 'Hello, world!'}
 
 
-# TODO: create a POST on a different path that does model inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
