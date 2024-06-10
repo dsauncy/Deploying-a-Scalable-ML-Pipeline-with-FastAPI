357547005c6fb47e0560bc368a750c92c721430b
dsauncy <dsauncy@wgu.edu>
2024-06-10 16:20:48 -0700

    Struggling to figure out why it's not iterating correctly over each feature, maybe?  Slice_output.txt shows the same scores over and over, cannot figure out why!

--- ml/model.py
+++ ml/model.py
@@ -1,8 +1,7 @@
 import pickle
+import numpy as np
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-
-####### CHECK IF LOGISTIC REGRESSION MODEL IS OKAY? #########
 from sklearn.linear_model import LogisticRegression
 
 # Optional: implement hyperparameter tuning.
@@ -125,17 +124,29 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
+    print("Slice Value:", slice_value)
+    slice_data = data[data[column_name] == slice_value]
+    print("Slice Data Shape:", slice_data.shape)
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value
-        # use training = False
-        data,
+        slice_data,
         categorical_features=categorical_features,
         label=label,
         training=False,
         encoder=encoder,
         lb=lb
     )
+
+    # The following is for debugging
+    print("X_slice Shape:", X_slice.shape)
+    print("y_slice Shape:", y_slice.shape)
+    print("Unique Labels in y_slice:", np.unique(y_slice))
+    print("Model Input Shape:", X_slice.shape)
+
     preds = inference(model, X_slice)
+
+    #more debugging
+    print("Predictions Shape:", preds.shape)
+    print("Unique Predictions:", np.unique(preds))
+
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
